e354551eb1ca38a17bc8fc5a39799383b1b25055
Shuvo Chandro Paul <shuvo.cp247@gmail.com>
2024-01-18 15:04:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+let fullName = 'Shuvo Chandro Paul';
+let age = 21;
+const isMarried = false;
+
+document.getElementById('full-name').textContent = fullName;
+document.getElementById('age').textContent = `His age is ${age} year old.`;
+document.getElementById(
+  'marital-status'
+).textContent = `Marital Status: ${isMarried}`;
+*/
+
+/* 
+    ? Operator Presedence
+        * 1. Parenthesis
+        * 2. exponents
+        * 3. Multiplication, Division & Modulas 
+        * 4. Addition & Subtraction
+*/
+
+/*
+let userName = window.prompt('Write your username?');
+document.getElementById('hello').textContent = `Hello ${userName}`;
+*/
+
+/*
+let userName;
+let myBtn = document.getElementById('mybtn');
+myBtn.onclick = function () {
+  userName = document.getElementById('user-name').value;
+  document.getElementById('welcome').textContent = `Welcome ${userName}`;
+};
+*/
